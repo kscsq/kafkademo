009a43c7e81db6b6f147bf478e9626ee1fec3acb
FYuDmitriev <fedor_dmitriev@rgs.ru>
2021-10-12 22:38:32 +0300

    initial commit

---
+++ src/test/java/ru/fedordmitriev/kafkademo/KafkademoApplicationTests.java
@@ -0,0 +1,12 @@
+package ru.fedordmitriev.kafkademo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkademoApplicationTests {
+
+    @Test void contextLoads() {
+    }
+
+}
